2ed958c343ee85d2b5a9ef5130b2098c85e1d37b
jonsan <gaigaisang@outlook.com>
2024-10-12 22:02:46 +0800

    up

--- algo/divide_and_conquer/select.h
+++ algo/divide_and_conquer/select.h
@@ -9,10 +9,11 @@ int j;
 int Select(int a[], int p, int r, int k) {
 //    第一个等于最后一个
     if (p == r)return a[p];
+//    i为枢轴将数据分为左右两部分
     int i = partition(a, p, r);
     j = i - p + 1;
     if (k <= j)
         return Select(a, p, i, k);
     else
-        return Select(a, p, i + 1, k - j);
+        return Select(a, i + 1, r, k - j);
 }
